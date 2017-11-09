c52114b7688be2fc299f0a34ee5c64c5d27dc1f5
Kimat Boven <kimat@users.noreply.github.com>
2017-11-09 10:28:00 +0100

    it should be easy to select what to clip (clipped or selected)

--- clip.sh
+++ clip.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
-FOLDER=/dev/shm/clipshed
-FILENAME=${FOLDER}/$(date +%s)
+CLIP_CMD="xclip -selection -c -o"
+# CLIP_CMD="xclip -o" # use this to watch any selection
+FOLDER="/dev/shm/clipshed"
+FILENAME="${FOLDER}/$(date +%s)"
 
 # compute clip MD5
-CLIP_MD5=$(xclip -selection c -o | md5sum | awk '{print $1}')
+CLIP_MD5=$($CLIP_CMD | md5sum | awk '{print $1}')
 echo "$CLIP_MD5"
 
 # last clip is still the clipboard
@@ -17,13 +19,13 @@ LAST_CLIP_MD5=$(md5sum "$LAST_CLIP" |  awk '{print $1}')
 [ "$LAST_CLIP_MD5" == "$CLIP_MD5" ] && echo "clip didnt change" && exit 0
 
 # check if already clipped
-ALREADY_CLIPPED=$(find ${FOLDER} -type f -0 | xargs -I{} md5sum {} |  grep ${CLIP_MD5})
-echo $ALREADY_CLIPPED
+ALREADY_CLIPPED=$(find $FOLDER -type f -0 | xargs -I{} md5sum {} |  grep "$CLIP_MD5")
+echo "$ALREADY_CLIPPED"
 
-if [ ! -z "${ALREADY_CLIPPED}" ]; then
+if [ ! -z "$ALREADY_CLIPPED" ]; then
   echo 'promoting duplicate'
-  mv "$(echo "$ALREADY_CLIPPED" | awk '{print $2}')" $FILENAME
+  mv "$(echo "$ALREADY_CLIPPED"| awk '{print $2}')" "$FILENAME"
 else
   echo "adding new clip"
-  xclip -selection c -o > $FILENAME
+  $CLIP_CMD > "$FILENAME"
 fi
